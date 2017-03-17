ad4ab59519c98d3deb7bbf00398a9bfdb7fea46e
Kostya Esmukov <kostya@esmukov.ru>
2017-03-17 09:37:07 +0300

    Added generated settings activity

--- app/src/main/java/ru/esmukov/kpfu/lightningrodandroid/MainActivity.java
+++ app/src/main/java/ru/esmukov/kpfu/lightningrodandroid/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import java.util.List;
@@ -102,4 +104,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
